910852136c0efcdf603021dc7e59fc68d6473134
Lobo <ezad821q@hotmail.com>
2024-11-08 10:07:57 -0300

    se probo modularizar los mapas con exito

---
+++ src/components/MapSalaCarrier.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container__map">
+        <div class="map">
+            
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
