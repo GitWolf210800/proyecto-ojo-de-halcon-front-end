4f238ad010b6e237c6f8d6942c6b28d80e99fbc4
Lobo <ezad821q@hotmail.com>
2025-01-02 09:33:58 -0300

    se mejora logica formulario Login de la app

---
+++ src/components/icons/LogoutButtom.vue
@@ -0,0 +1,90 @@
+<template>
+  <svg
+    width="20.187481mm"
+    height="7.8552585mm"
+    viewBox="0 0 20.187481 7.8552585"
+    version="1.1"
+    id="svg54677"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:svg="http://www.w3.org/2000/svg"
+  >
+    <defs id="defs54674">
+      <rect x="1010.5" y="103.5" width="82" height="21" id="rect1682" />
+    </defs>
+    <g
+      id="layer1"
+      transform="matrix(0.65329173,0,0,0.62775903,-48.278997,-57.693856)"
+    >
+      <rect
+        style="
+          display: inline;
+          fill: #c00009;
+          fill-opacity: 0.86685503;
+          stroke: #5c0004;
+          stroke-width: 1.43382;
+          stroke-linecap: round;
+          stroke-linejoin: round;
+          stroke-dasharray: none;
+          stroke-opacity: 0.86666662;
+          paint-order: stroke fill markers;
+        "
+        id="loginButtonBack"
+        width="29.130749"
+        height="10.986405"
+        x="73.955139"
+        y="93.032898"
+        transform="matrix(0.99999164,-0.00408853,0.00797366,0.99996821,0,0)"
+        ry="3.1580486"
+      />
+      <text
+        xml:space="preserve"
+        transform="matrix(0.51902146,0,0,0.42016862,-446.28047,51.015976)"
+        id="loginTextButton"
+        style="
+          font-style: normal;
+          font-variant: normal;
+          font-weight: normal;
+          font-stretch: normal;
+          font-size: 14.6667px;
+          font-family: Dubai;
+          -inkscape-font-specification: 'Dubai, Normal';
+          font-variant-ligatures: normal;
+          font-variant-caps: normal;
+          font-variant-numeric: normal;
+          font-variant-east-asian: normal;
+          white-space: pre;
+          shape-inside: url(#rect1682);
+          display: inline;
+          fill: #f4f9fb;
+          fill-opacity: 0.866855;
+          stroke: #f0f6fc;
+          stroke-width: 0;
+          stroke-opacity: 1;
+        "
+      >
+        <tspan x="1010.5" y="116.4064" id="tspan2310">Logout</tspan>
+      </text>
+      <rect
+        style="
+          display: inline;
+          fill: #964343;
+          fill-opacity: 0.0849858;
+          stroke: #000000;
+          stroke-width: 0.577576;
+          stroke-linecap: round;
+          stroke-linejoin: round;
+          stroke-dasharray: none;
+          stroke-opacity: 0.0934844;
+          paint-order: stroke fill markers;
+        "
+        id="loginButton"
+        width="30.284918"
+        height="11.559542"
+        x="73.606934"
+        y="92.69841"
+        transform="matrix(0.99999144,-0.00413788,0.00787854,0.99996896,0,0)"
+        ry="3.3227975"
+      />
+    </g>
+  </svg>
+</template>
