86638a5cf24a8b3679c34d089a90a984c9173d3b
Lobo <ezad821q@hotmail.com>
2025-01-03 08:05:12 -0300

    se modifica componente para loggear

---
+++ src/components/ButtonLog.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script setup>
+import { watch } from 'vue';
+const logeado = JSON.parse(localStorage.getItem('sesion'));
+console.log(logeado);
+console.log(typeof logeado);
+
+//watch()
+
+</script>
+
+<style scoped>
+
+</style>
