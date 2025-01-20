4368e3618c40403b0b8228c230a1115807393d7f
Lobo <ezad821q@hotmail.com>
2025-01-20 11:09:12 -0300

    se añade vue-cookies

--- src/components/ButtonLog.vue
+++ src/components/ButtonLog.vue
@@ -6,15 +6,15 @@
       <h1>Login Ojo de Halcón</h1>
       <form @submit.prevent="handleSubmit">
         <label for="username">
-          <i class="fas fa-user"></i>
+          <fa icon="user"/>
         </label>
         <input v-model="form.legajo" id="username" type="text" placeholder="Legajo" required />
         <label for="password">
-          <i class="fas fa-lock"></i>
+           <fa icon="lock"/> 
         </label>
         <input v-model="form.contraseña" id="password" type="password" placeholder="Password" required />
   
-        <p class="error" v-if="error">Error en Inicio de Sesión</p>
+        <p class="error" v-if="error" id="error" >Error en Inicio de Sesión</p>
         <input type="submit" value="Ingresar" />
       </form>
     </div>
@@ -42,16 +42,18 @@
   };
   
   const logout = () => {
+    //document.cookie = 'jwt=; path=/; Expires=thu, 01 Jan 1970 00:00:01 GMT;';
     localStorage.removeItem('sesion');
+    console.log(document.cookie);
     loginTrue.value = false;
   };
   
   const handleSubmit = async () => {
     error.value = null;
   
-    const { legajo, contraseña } = form;
+    const { legajo, contraseña,  } = form;
     if (!legajo || !contraseña) {
-      error.value = 'Por favor, complete todos los campos';
+      error.value = true;
       return;
     }
   
@@ -71,6 +73,8 @@
       const data = await res.json();
       localStorage.setItem('sesion', JSON.stringify(data.usuario));
       loginTrue.value = !!data.usuario.name;
+      form.legajo = '';
+      form.contraseña = '';
       visibilityForm.value = !loginTrue.value;
     } catch (err) {
       console.error('Error:', err);
@@ -88,27 +92,66 @@
     width: 400px;
     background-color: #ffffff;
     box-shadow: 0 0 9px 0 rgba(0, 0, 0, 0.3);
+    display: flex;
+    flex-direction: column; /* Asegura que el título esté encima del formulario*/
     position: fixed;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     z-index: 1600;
+    overflow: hidden; /* Opcional: para asegurar que el contenido no se desborde */ 
   }
   .login h1 {
     text-align: center;
     color: #5b6574;
     font-size: 24px;
     padding: 20px 0;
+    margin: 0;
+    border-bottom: 1px solid #dee0e4;
+    background-color: #f9f9f9 /* Fondo opcional para resaltar el título */;
+    width: 100% /* Asegura que ocupe todo el ancho */;
   }
   .login form {
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
-    padding: 20px 0;
+    padding-top: 20px;
   }
-  .login form input {
+  .login form label {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50px;
+    height: 50px;
+    background-color: #1984bc;
+    color: #ffffff;
+  }
+  .login form input[type="password"], .login form input[type="text"] {
+    width: 310px;
+    height: 50px;
+    border: 1px solid #dee0e4;
+    margin-bottom: 20px;
+    padding: 0 15px;
+  }
+  .login form input[type="submit"] {
+    width: 100%;
+    padding: 15px;
+    margin-top: 20px;
+    background-color: #1984bc;
+    border: 0;
+    cursor: pointer;
+    font-weight: bold;
+    color: #ffffff;
+    transition: background-color 0.2s;
+  }
+  .login form input[type="submit"]:hover {
+    background-color: #1984bc;
+    transition: background-color 0.2s;
+  }
+
+  /*.login form input {
     margin-bottom: 15px;
     padding: 10px;
-  }
+  }*/
   </style>
   
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue';
 import '@/assets/main.css';
 import router from './router';
 import { isMobile } from './funciones';
+import VueCookies from 'vue-cookies';
 
 // Font Awesome
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
@@ -168,6 +169,7 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VueCookies);
 app.component("fa", FontAwesomeIcon);
 app.directive('zoom', zoomDirective);
 
