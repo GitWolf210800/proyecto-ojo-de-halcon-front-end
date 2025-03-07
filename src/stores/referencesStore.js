24f4e25c76730cfe73061250c8a6cd1aa703f2d6
Lobo <ezad821q@hotmail.com>
2025-03-07 09:44:47 -0300

    cambio de null a objeto vacio store de referencia

--- src/stores/referencesStore.js
+++ src/stores/referencesStore.js
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const useReferenceStore = defineStore('dataUserStore', () => {
-    const reference = ref(null); //Aqui se almacenaran las referencias de los eventos que se vayan a crear
+    const reference = ref({}); //Aqui se almacenaran las referencias de los eventos que se vayan a crear
 
     const setReference = (referenceElement) => {
         reference.value = referenceElement;
