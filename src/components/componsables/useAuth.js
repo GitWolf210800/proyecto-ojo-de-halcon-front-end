a8276ad3e388c4fb2ef45fad1d7247aef603c4d3
Lobo <ezad821q@hotmail.com>
2025-03-17 12:08:59 -0300

    prueba

--- src/components/componsables/useAuth.js
+++
@@ -1,8 +0,0 @@
-/*import { useAuthStore } from "@/stores/authStore";
-//import { getUSerCookie } from "@/helpers/getCookieUser";
-
-export const useDataAuthUser = async () => {
-    //const datosUser = await getUSerCookie();
-    const datoUserStore = useAuthStore();
-    datoUserStore.datos = datosUser;
-};*/
