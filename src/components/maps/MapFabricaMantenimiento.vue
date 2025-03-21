fe1373ef2161bd6e8754ccfef38d77282a00825f
Lobo <ezad821q@hotmail.com>
2025-03-21 12:05:00 -0300

    mapa mantenimiento semiterminado

--- src/components/maps/MapFabricaMantenimiento.vue
+++ src/components/maps/MapFabricaMantenimiento.vue
@@ -3,26 +3,83 @@
         <MapMantenimiento ref="mapMantenimientoRef" class="container__map" />
     </div>
 
+    <ToolTipChartInfo 
+        :position="tooltipPosition"
+        :parametros="params"
+        v-if="tooltipVisibility.chartInfo"
+    />
 </template>
 
 <script setup>
 import { computed, onMounted, ref, watchEffect } from 'vue';
 import MapMantenimiento from './MapMantenimiento.vue';
-import { useHomeMantenimientoStore } from '@/stores/homeMantenimientoStore';
 import { dataColorInfoMantenimiento } from '@/helpers/homeMantenimientoManipulatorColor';
+
+import { useHomeMantenimientoStore } from '@/stores/homeMantenimientoStore';
+import { useReferenceStore } from '@/stores/referencesStore';
+
+import { useTooltip } from "@/modules/tooltip/utils/useTooltip";
+import ToolTipChartInfo from "@/modules/tooltip/components/ToolTipChartInfo.vue";
+
+import { createTooltipConfig } from '@/funciones';
+import {
+  TOOLTIP_CHART_CONFIG,
+  TOOLTIP_CHART_INFO_CONFIG,
+  TOOLTIP_INFO_TABLE,
+} from "@/variables.js";
+
+// Se utiliza el composable `useTooltip`
+const { tooltipPosition, params, tooltipVisibility, displayTooltip, hideTooltip } = useTooltip();
 
 const mapMantenimientoRef = ref(null);
 let svg = null;
 const storeData = ref(useHomeMantenimientoStore().datos);
+const referenceStorage = ref({});
 
 const homeMantenimientoStore = useHomeMantenimientoStore();
+const referenceStore = useReferenceStore();
 
 const dataIsLoaded = computed(() => homeMantenimientoStore.datos !== null);
 const svgIsLoaded = computed(() => mapMantenimientoRef.value !== null);
 
+function initializeTooltipEvents(svg){
+    const tooltipConfigs = [
+        ...storeData.value.compresores.map((dato) => 
+            createTooltipConfig(`#${dato.nombre}`, 'chartInfo', { nombre: dato.nombre, medicion: 'presion_salida_compresor', tabla: 'mediciones_compresores' }, TOOLTIP_CHART_INFO_CONFIG)
+        )
+    ];
+
+    //console.log(tooltipConfigs);
+
+    tooltipConfigs.forEach(({ selector, tooltipType, payload, config }) => {
+        const element = svg.querySelector(selector);
+
+        if (element) {
+            const handlerON = (e) => displayTooltip(e, tooltipType, payload, config);
+            const handlerOff = () => hideTooltip(tooltipType);
+
+            element.addEventListener('mouseover', handlerON);
+            element.addEventListener('mouseleave', handlerOff);
+
+            try{
+                referenceStorage.value[selector] = referenceStorage.value[selector] || {};
+                referenceStorage.value[selector]['mouseover'] = handlerON;
+                referenceStorage.value[selector]['mouseleave'] = handlerOff;
+            } catch {
+                console.log('error en: ', selector);
+            }
+        }
+    });
+
+    referenceStore.setReference(referenceStorage.value);
+};
+
+
 onMounted(() => {
     if(mapMantenimientoRef.value.svgRef){
         svg = mapMantenimientoRef.value.svgRef;
+
+        initializeTooltipEvents(mapMantenimientoRef.value.svgRef);
     }
 });
 
