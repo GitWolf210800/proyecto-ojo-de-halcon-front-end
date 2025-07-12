b101bbdb5df0daa30f241f07e51445793b8e242a
Lobo <ezad821q@hotmail.com>
2025-07-12 11:08:47 -0300

    se deja como estaba

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,24 +3,8 @@
         <ButtonLog />
     </div>
 
-    <div class="logo">
-        <IconoOjodeHalcon />
-    </div>
 </template>
 
 <script setup>
 import ButtonLog from '@/components/componentesVue/ButtonLog.vue';
-import IconoOjodeHalcon from '@/components/icons/IconoOjodeHalcon.vue';
 </script>
-
-<style scoped>
-.logo {
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    margin: 10px;
-    width: 15vw;
-    max-width: 80px;
-    height: auto;
-  }
-</style>
