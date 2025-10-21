a167476e50bcf7bad927ff429f5d71cf0a4c6a51
Lobo <ezad821q@hotmail.com>
2025-10-21 10:01:06 -0300

    armado de componente de edicion de compresores

---
+++ src/components/componentesVue/EstadoCompresor.vue
@@ -0,0 +1,28 @@
+<template>
+    <EditarEstadoCompresorButtom 
+        class="icon-container"
+        v-if="show && esRutaEspecifica"
+    />
+</template>
+
+<script setup>
+import EditarEstadoCompresorButtom from '../icons/EditarEstadoCompresorButtom.vue';
+import { ref, computed } from 'vue';
+
+const show = ref(false);
+const esRutaEspecifica = computed(() => route.path === "/mantenimiento");
+
+const toggle = () => {
+
+};
+</script>
+
+<style scoped>
+
+.icon-container {
+  position: relative;
+  display: inline-block;
+  cursor: pointer;
+}
+
+</style>
